04f3ec8dd0c7289f4333e012e34cca99e7d634ab
Ryan Flores <ryan.designe.ux@outlook.com>
2022-05-11 01:11:13 -0300

    Logica de lojas finalizada

--- src/vendas/routes.js
+++ src/vendas/routes.js
@@ -3,7 +3,11 @@ const controller = require('./controller');
 
 const router = Router();
 
+//Rotas de lojas
 router.get('/', controller.getLojas);
+router.post('/', controller.addLojas);
 router.get('/:id', controller.getLojasById);
+router.delete('/:id', controller.deleteLoja);
+router.put('/:id', controller.updateLoja);
 
 module.exports = router;
